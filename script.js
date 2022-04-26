d724e4af6ddde4b055b6cb57d3982e75d8f21f20
andzejzi <azilinskij@gmail.com>
2022-04-26 22:46:00 +0300

    first lesson

---
+++ script.js
@@ -0,0 +1,15 @@
+//  One line komentaras
+
+/*
+Sveiki čia yra 
+Multi line komentaras
+ */
+
+// Sukuriam kintamuosius
+let myNameSurname;
+let age;
+
+// Priskiriam kintamiejam reikšmes
+
+myName = "Andžej";
+age = 40;
